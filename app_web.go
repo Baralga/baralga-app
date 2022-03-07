ccab3cd645316c7af5dcbc175d9ba833594e0409
Jan Stamer <914776+remast@users.noreply.github.com>
2022-03-07 11:47:46 +0100

    feat: updated htmx 1.6.1 to 1.7.0

--- app_web.go
+++ app_web.go
@@ -405,7 +405,7 @@ func Page(title, currentPath string, body []g.Node) g.Node {
 				g.Attr("defer", "defer"),
 			),
 			Script(
-				Src("/assets/htmx-1.6.1/htmx.min.js"),
+				Src("/assets/htmx-1.7.0/htmx.min.js"),
 				g.Attr("crossorigin", "anonymous"),
 				g.Attr("defer", "defer"),
 			),
