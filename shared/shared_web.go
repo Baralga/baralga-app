4002ec626ebf62a3b183e214fd7102bd57163414
Jan Stamer <914776+remast@users.noreply.github.com>
2023-04-30 19:35:32 +0200

    Bootstrap updates.

--- shared/shared_web.go
+++ shared/shared_web.go
@@ -86,13 +86,13 @@ func Page(title, currentPath string, body []g.Node) g.Node {
 			),
 			Link(
 				Rel("stylesheet"),
-				Href("/assets/bootstrap-5.3.0-alpha1/bootstrap.min.css"),
-				g.Attr("integrity", "sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"),
+				Href("/assets/bootstrap-5.3.0-alpha3/bootstrap.min.css"),
+				g.Attr("integrity", "sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"),
 				g.Attr("crossorigin", "anonymous"),
 			),
 			Link(
 				Rel("stylesheet"),
-				Href("/assets/bootstrap-icons-1.10.2/bootstrap-icons.css"),
+				Href("/assets/bootstrap-icons-1.10.5/bootstrap-icons.min.css"),
 				g.Attr("media", "print"),
 				g.Attr("onload", "this.media='all'"),
 				g.Attr("crossorigin", "anonymous"),
@@ -110,9 +110,9 @@ func Page(title, currentPath string, body []g.Node) g.Node {
 				Href("manifest.webmanifest"),
 			),
 			Script(
-				Src("/assets/bootstrap-5.3.0-alpha1/bootstrap.bundle.min.js"),
+				Src("/assets/bootstrap-5.3.0-alpha3/bootstrap.bundle.min.js"),
 				g.Attr("crossorigin", "anonymous"),
-				g.Attr("integrity", "sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"),
+				g.Attr("integrity", "sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"),
 				g.Attr("defer", "defer"),
 			),
 			Script(
