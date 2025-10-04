0cff89355b1159e29d11e082953fada79c251522
Jan Stamer <914776+remast@users.noreply.github.com>
2025-10-04 14:22:20 +0200

    consolidate tag scripts

--- shared/migrations/000006_add_tag_color.up.sql
+++
@@ -1,6 +0,0 @@
--- Add color column to tags table
-ALTER TABLE tags
-ADD COLUMN color varchar(7) NOT NULL DEFAULT '#6c757d';
-
--- Update existing tags with default color
-UPDATE tags SET color = '#6c757d' WHERE color IS NULL;
